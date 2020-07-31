d44c27fcb60606563c876a242f8d4801bc205710
Taylor Chen <s68175@gmail.com>
2020-07-31 23:39:07 +0800

    Add login check

---
+++ loginCheck.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+function loginCheck(email, password) {
+  const user = users.find(element => element.email && password === element.password)
+  return user ? user.firstName : 'loginFail'
+}
+
+module.exports = loginCheck
